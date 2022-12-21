7d9bcee638cab5a2e21f72ecbec5bf29cd344fb3
omar saadeh <omarsaadeh11@gmail.com>
2022-12-21 11:56:20 -0800

    Restructuring the project

--- routes/customers.js
+++ routes/customers.js
@@ -1,36 +1,22 @@
-const mongoose = require("mongoose");
-const Joi = require("joi");
+const { Customer, validate } = require("../models/customer");
+//.Customer
 const express = require("express");
 const router = express.Router();
 
-const customerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-  isGold: {
-    type: Boolean,
-    default: false,
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-});
+/*
 
-const Customer = mongoose.model("Customer", customerSchema);
 
+hon kenet el schema...
+
+
+*/
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort("name");
   res.send(customers);
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({
@@ -44,12 +30,12 @@ router.post("/", async (req, res) => {
 });
 
 router.put("/:id", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findByIdAndUpdate(
     req.params.id,
-    { name: req.body.name },
+    { name: req.body.name, isGold: req.body.isGold, phone: req.body.phone },
     { new: true }
   );
 
@@ -82,15 +68,10 @@ router.get("/:id", async (req, res) => {
 
   res.send(customer);
 });
+/* 
+hon kenet el
+joi validation
 
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean(),
-  };
-
-  return Joi.validate(customer, schema);
-}
+*/
 
 module.exports = router;
